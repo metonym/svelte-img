fe0c72d72c8c54d518f306c34a62f2dce2b76b32
Eric Liu <ericyl.us@gmail.com>
2021-08-04 05:55:47 -0700

    chore: create separate test folder

--- test/Img.test.svelte
+++ test/Img.test.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import Img from "./types";
+  import Img from "../types";
   import { tick } from "svelte";
 
-  let url = "https://upload.wikimedia.org/wikipedia/commons/a/a8/Nighthawks_by_Edward_Hopper_1942.jpg";
+  let url =
+    "https://upload.wikimedia.org/wikipedia/commons/a/a8/Nighthawks_by_Edward_Hopper_1942.jpg";
   let src = url;
 
   function reload() {
@@ -23,10 +24,11 @@
   }}
   on:error={(e) => {
     console.log(e.detail);
-  }}>
+  }}
+>
   <div slot="loading">Loading...</div>
   <div slot="error">An error occurred.</div>
 </Img>
 
 <button type="button" on:click={reload}>Reload</button>
-<button type="button" on:click={() => (src = url + '1')}>Simulate error</button>
+<button type="button" on:click={() => (src = url + "1")}>Simulate error</button>
